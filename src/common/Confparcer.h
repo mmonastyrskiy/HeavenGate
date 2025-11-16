451954a66fafaa01f784241872fcbfa15ddc0696
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2025-11-17 00:53:02 +0300

    fix

--- src/common/Confparcer.h
+++ src/common/Confparcer.h
@@ -37,9 +37,10 @@ template<typename T>
 static T SETTING(const std::string& sett, const T& default_value = T{}) {
     std::string arg = Argparcer::Argparcer::the().get(sett);
     LOG_DEBUG("gOT VALUES FROM ARGS " +arg);
+    int e = 0;
     auto confparcer = Confparcer::the();
     confparcer.parce();
-    auto conf = confparcer.get(sett);
+    auto conf = confparcer.get(sett,&ef);
     LOG_DEBUG("GOT VALUES FROM CONFIG AND ARGS " + conf);
     
     std::string value;
